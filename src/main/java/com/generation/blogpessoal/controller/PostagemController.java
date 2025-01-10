5cd9b1dc25b3b98e3bdef970d7fee150359ac44b
hixdann <danilo.f.almeida@outlook.com>
2025-01-10 15:19:21 -0300

    Adicionado alguns comentários

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -12,17 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-@RestController
-@RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController   // Define ao Spring que essa classe é uma Controller
+@RequestMapping("/postagens") // Define qual endpoint vai ser tratado por essa Classe
+@CrossOrigin(origins = "*", allowedHeaders = "*")  //Libera o acesso a qualquer front que não | Se quisermos definir os dominios a ser acessado usamos entre aspas o endereço, Ex: www.generation.com/front
 public class PostagemController {
 	
-	@Autowired
+	@Autowired  // O spring dá autonomia para a Interface poder invocar os métodos
 	private PostagemRepository postagemRepository;
 	
-	@GetMapping
+	@GetMapping  // Indica que esse método é chamado em Vérbos/Métodos HTTP do tipo Get
 	public ResponseEntity<List<Postagem>> getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll());
+		return ResponseEntity.ok(postagemRepository.findAll()); // SELECT * FROM tb_postagens
 	}
 
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -5,5 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	
+	// save(Objeto objeto)
+	
+	// findById (Long id)
+	
+	// existingById(Long id)
+	
+	// findAll()
+	
+	// deleteById(Long id)
+	
+	// deleteAll()
 
 }
